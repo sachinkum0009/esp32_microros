45d144a1ab8b1a1529a7e730878e7197e6b4e7f1
Sachin Kumar <sachinkum123567@gmail.com>
2024-07-12 12:57:52 +0200

    mpu6050 imu publisher added

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,40 @@
 #include <std_msgs/msg/int32_multi_array.h>
 #include <geometry_msgs/msg/twist.h>
 
+#define NANO_SECS         1000000000
 
+// #include <MPU6050.h>
+#include <I2Cdev.h>
+#include "MPU6050_6Axis_MotionApps20.h"
+
+#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+    #include "Wire.h"
+#endif
+
+MPU6050 mpu;
+
+#define EARTH_GRAVITY_MS2 9.80665  // m/s2
+#define DEG_TO_RAD        0.017453292519943295769236907684886
+#define RAD_TO_DEG        57.295779513082320876798154814105
+
+
+// MPU control/status vars
+bool dmpReady = false;  // set true if DMP init was successful
+uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
+uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
+uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
+uint16_t fifoCount;     // count of all bytes currently in FIFO
+uint8_t fifoBuffer[64]; // FIFO storage buffer
+
+// orientation/motion vars
+Quaternion q;           // [w, x, y, z]         quaternion container
+VectorInt16 aa;         // [x, y, z]            accel sensor measurements
+VectorInt16 gg;         // [x, y, z]            gyro sensor measurements
+VectorInt16 aaWorld;    // [x, y, z]            world-frame accel sensor measurements
+VectorInt16 ggWorld;    // [x, y, z]            world-frame accel sensor measurements
+VectorFloat gravity;    // [x, y, z]            gravity vector
+float euler[3];         // [psi, theta, phi]    Euler angle container
+float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
 
 // #if !defined(MICRO_ROS_TRANSPORT_ARDUINO_SERIAL)
 // #error This example is only avaliable for Arduino framework with serial transport.
@@ -44,6 +77,69 @@ void error_loop() {
 void timer_callback(rcl_timer_t * timer, int64_t last_call_time) {
   RCLC_UNUSED(last_call_time);
   if (timer != NULL) {
+    /**
+     * IMU Sensor Values
+     */
+    if (!dmpReady) return;
+    if (mpu.dmpGetCurrentFIFOPacket(fifoBuffer)) { // Get the Latest packet 
+
+        // display quaternion values in easy matrix form: w x y z
+        mpu.dmpGetQuaternion(&q, fifoBuffer);
+        // Serial.print("quat\t");
+        // Serial.print(q.w);
+        // Serial.print("\t");
+        // Serial.print(q.x);
+        // Serial.print("\t");
+        // Serial.print(q.y);
+        // Serial.print("\t");
+        // Serial.println(q.z);
+
+        mpu.dmpGetGravity(&gravity, &q);
+
+        // display initial world-frame acceleration, adjusted to remove gravity
+        // and rotated based on known orientation from quaternion
+        mpu.dmpGetAccel(&aa, fifoBuffer);
+        mpu.dmpConvertToWorldFrame(&aaWorld, &aa, &q);
+        // Serial.print("aworld\t");
+        // Serial.print(aaWorld.x * mpu.get_acce_resolution() * EARTH_GRAVITY_MS2);
+        // Serial.print("\t");
+        // Serial.print(aaWorld.y * mpu.get_acce_resolution() * EARTH_GRAVITY_MS2);
+        // Serial.print("\t");
+        // Serial.println(aaWorld.z * mpu.get_acce_resolution() * EARTH_GRAVITY_MS2);
+
+        // display initial world-frame acceleration, adjusted to remove gravity
+        // and rotated based on known orientation from quaternion
+        mpu.dmpGetGyro(&gg, fifoBuffer);
+        mpu.dmpConvertToWorldFrame(&ggWorld, &gg, &q);
+        // Serial.print("ggWorld\t");
+        // Serial.print(ggWorld.x * mpu.get_gyro_resolution() * DEG_TO_RAD);
+        // Serial.print("\t");
+        // Serial.print(ggWorld.y * mpu.get_gyro_resolution() * DEG_TO_RAD);
+        // Serial.print("\t");
+        // Serial.println(ggWorld.z * mpu.get_gyro_resolution() * DEG_TO_RAD);
+
+        mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
+        // Serial.print("ypr\t");
+        // Serial.print(ypr[0] * RAD_TO_DEG);
+        // Serial.print("\t");
+        // Serial.print(ypr[1] * RAD_TO_DEG);
+        // Serial.print("\t");
+        // Serial.println(ypr[2] * RAD_TO_DEG);
+        
+        // Serial.println();
+
+    }
+    imu_msg.header.stamp.sec = (int32_t)(rmw_uros_epoch_nanos() / NANO_SECS);
+    imu_msg.header.stamp.nanosec = (uint32_t)(rmw_uros_epoch_nanos() % NANO_SECS);
+    imu_msg.orientation.x = q.x;
+    imu_msg.orientation.y = q.y;
+    imu_msg.orientation.z = q.z;
+    imu_msg.orientation.w = q.w;
+
+    imu_msg.linear_acceleration.x = aaWorld.x;
+    imu_msg.linear_acceleration.y = aaWorld.y;
+    imu_msg.linear_acceleration.z = aaWorld.z;
+
     RCSOFTCHECK(rcl_publish(&publisher, &msg, NULL));
     RCSOFTCHECK(rcl_publish(&imu_publisher, &imu_msg, NULL));
     RCSOFTCHECK(rcl_publish(&encoder_publisher, &encoder_msg, NULL));
@@ -59,7 +155,53 @@ void subscription_callback(const void * msgin)
         printf("Received linear x: %f, angular z: %f \n",  (float)  msg->linear.x, (float) msg->angular.z);
 }
 
+void initialize_ros_msgs()
+{
+    // imu_msg.header.frame_id.capacity = 100; 
+    // imu_msg.header.frame_id.size = 100;
+    // imu_msg.header.frame_id.data = "imu_link".to_char(); // (char *) malloc(100 * sizeof(char)) ;
+    // imu_msg.header.frame_id = "imu_link";
+    char* frame_id = new char[strlen("imu_link") + 1];
+    strcpy(frame_id, "imu_link");
+    imu_msg.header.frame_id.data = frame_id;
+}
+
 void setup() {
+
+
+    #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+        Wire.begin();
+        Wire.setClock(400000); // 400kHz I2C clock. Comment this line if having compilation difficulties
+    #elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
+        Fastwire::setup(400, true);
+    #endif
+
+
+    mpu.initialize();
+    devStatus = mpu.dmpInitialize();
+
+    // supply your own gyro offsets here, scaled for min sensitivity
+    mpu.setXGyroOffset(-156);
+    mpu.setYGyroOffset(-11);
+    mpu.setZGyroOffset(-14);
+    mpu.setXAccelOffset(-3699);
+    mpu.setYAccelOffset(-2519);
+    mpu.setZAccelOffset(1391); // 1688 factory default for my test chip
+
+    // make sure it worked (returns 0 if so)
+    if (devStatus == 0) {
+
+        mpu.CalibrateAccel(6);
+        mpu.CalibrateGyro(6);
+        mpu.PrintActiveOffsets();
+        mpu.setDMPEnabled(true);
+
+        mpuIntStatus = mpu.getIntStatus();
+
+        dmpReady = true;
+        packetSize = mpu.dmpGetFIFOPacketSize();
+    }
+
   // Configure serial transport
   Serial.begin(115200);
   set_microros_serial_transports(Serial);
@@ -74,6 +216,7 @@ void setup() {
 
 
   delay(2000);
+  initialize_ros_msgs();
 
   allocator = rcl_get_default_allocator();
 
@@ -129,6 +272,11 @@ RCCHECK(rclc_executor_add_subscription(&executor, &twist_subscriber, &twist_msg,
 }
 
 void loop() {
+
+
+
+    
+
   delay(10);
   RCSOFTCHECK(rclc_executor_spin_some(&executor, RCL_MS_TO_NS(10)));
 }
